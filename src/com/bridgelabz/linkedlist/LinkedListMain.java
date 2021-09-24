2c4c06c95ef129cf5501eba738ef6a69d5a04162
aravindkarri <aravindkarri98495@gmail.com>
2021-09-24 20:29:21 +0530

    ADDED: created a simple linked list

---
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.linkedlist;
+
+public class LinkedListMain
+{
+    public static void main(String[] args)
+    {
+        LinkedListService<Integer>linkedListObj = new LinkedListService<Integer>();
+        linkedListObj.add(56);
+        linkedListObj.add(30);
+        linkedListObj.add(70);
+        linkedListObj.display();
+    }
+}
