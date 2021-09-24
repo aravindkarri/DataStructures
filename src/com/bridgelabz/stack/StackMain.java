65d666545f47d1fe85a8d1ac3a6e007bb62da065
aravindkarri <aravindkarri98495@gmail.com>
2021-09-25 01:13:01 +0530

    ADDED: stack created with linked list

---
+++ src/com/bridgelabz/stack/StackMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.stack;
+
+public class StackMain
+{
+   public static void main(String[] args)
+   {
+       StackService<Integer> stack = new StackService<>();
+       stack.push(70);
+       stack.push(30);
+       stack.push(56);
+       stack.print();
+   }
+}
---
+++ src/com/bridgelabz/stack/StackService.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.stack;
+import com.bridgelabz.linkedlist.LinkedListService;
+
+public class StackService<T extends Comparable<T>>
+{
+    LinkedListService<T> linkedList = new LinkedListService<T>();
+    public void push(T data)
+    {
+        linkedList.add(data);
+    }
+    public void print()
+    {
+        linkedList.display();
+    }
+}
